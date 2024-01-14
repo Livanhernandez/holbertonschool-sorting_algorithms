96cb71205e7aeb6fcce980dd144bba04b12f4c5f
Livanhernandez <livanhernandez9@gmail.com>
2024-01-13 20:58:08 -0400

    checkers

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,33 +1,34 @@
 #include "sort.h"
 
 /**
- * selection_sort - sorts array of int in ascending order
- * array: pointer to the array
- * size: pointer to the size of the array
+ * selection_sort- sorts array ints ascending order
+ * @array: array unsorted of ints
+ * @size: size of array
+ *
  */
 
 void selection_sort(int *array, size_t size)
 {
-	int minor;
-	size_t temp_indx = 0;
 	size_t i, j;
+	int min;
+	size_t index_min;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		minor = array[i];
-
-		for (j = i + 1; j < size; j++)
+		min = array[i]; /* suppose i is always min when starting */
+		index_min = i;
+		for (j = i + 1; j < size; j++) /* start one index over and iterate all */
 		{
-			if (array[j] < minor)
+			if (min > array[j])
 			{
-				minor = array[j];
-				temp_indx = j;
+				min = array[j]; /* save new min */
+				index_min = j; /* remember index we found min */
 			}
 		}
-		if (temp_indx != i)
+		if (index_min != i) /* if no new min found, indx_min = i, wont execute */
 		{
-			array[temp_indx] = array[i];
-			array[i] = minor;
+			array[index_min] = array[i]; /* switch value where indx min was found*/
+			array[i] = min; /* min set to lowest position in array available */
 			print_array(array, size);
 		}
 	}
