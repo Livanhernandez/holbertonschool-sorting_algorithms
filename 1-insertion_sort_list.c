9816265d967e7db00bfef13a829c47f581aad9c7
Livanhernandez <livanhernandez9@gmail.com>
2024-01-13 21:06:53 -0400

    fixed checkers

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,35 +7,35 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-        listint_t *current;
-        listint_t *temp;
+	listint_t *current;
+	listint_t *temp;
 
-        if (list == NULL || *list == NULL || (*list)->next == NULL)
-                return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-        current = (*list)->next;
+	current = (*list)->next;
 
-        while (current != NULL)
-        {
-                temp = current;
+	while (current != NULL)
+	{
+		temp = current;
 
-                while (temp->prev != NULL && temp->n < temp->prev->n)
-                {
-                        if (temp->next != NULL)
-                                temp->next->prev = temp->prev;
-                        temp->prev->next = temp->next;
-                        temp->next = temp->prev;
-                        temp->prev = temp->prev->prev;
-                        temp->next->prev = temp;
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
+			temp->prev->next = temp->next;
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
 
-                        if (temp->prev == NULL)
-                                *list = temp;
-                        else
-                                temp->prev->next = temp;
+			if (temp->prev == NULL)
+				*list = temp;
+			else
+				temp->prev->next = temp;
 
-                        print_list(*list);
-                }
+			print_list(*list);
+		}
 
-                current = current->next;
-        }
+		current = current->next;
+	}
 }
