3680ebcbd1b7735327b6fab3ab565cec582c9e79
Livanhernandez <livanhernandez9@gmail.com>
2024-01-13 20:44:39 -0400

    fixed conflict

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,4 +1,41 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list -
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ * using the Insertion sort algorithm.
+ * @list: Double pointer to the head of the doubly linked list
+ */
+void insertion_sort_list(listint_t **list)
+{
+        listint_t *current;
+        listint_t *temp;
+
+        if (list == NULL || *list == NULL || (*list)->next == NULL)
+                return;
+
+        current = (*list)->next;
+
+        while (current != NULL)
+        {
+                temp = current;
+
+                while (temp->prev != NULL && temp->n < temp->prev->n)
+                {
+                        if (temp->next != NULL)
+                                temp->next->prev = temp->prev;
+                        temp->prev->next = temp->next;
+                        temp->next = temp->prev;
+                        temp->prev = temp->prev->prev;
+                        temp->next->prev = temp;
+
+                        if (temp->prev == NULL)
+                                *list = temp;
+                        else
+                                temp->prev->next = temp;
+
+                        print_list(*list);
+                }
+
+                current = current->next;
+        }
+}
